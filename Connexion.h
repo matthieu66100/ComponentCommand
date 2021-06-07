cce299257ac5db3d47cb5fe6174b54b50ea9d1d4
Basto <matthieu.basto@fr.thalesgroup.com>
2021-06-07 13:58:59 +0200

    Mise en page Connexion et Hub

--- Connexion.h
+++ Connexion.h
@@ -34,12 +34,19 @@ namespace Commande_Composants {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Panel^ panel1;
-	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label2;
+
+	protected:
+
+
+
+
 
 
 
@@ -59,76 +66,90 @@ namespace Commande_Composants {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
-			this->panel1->SuspendLayout();
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// panel1
+			// button1
 			// 
-			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
-				| System::Windows::Forms::AnchorStyles::Left)
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->panel1->Controls->Add(this->comboBox1);
-			this->panel1->Controls->Add(this->textBox1);
-			this->panel1->Controls->Add(this->button1);
-			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(12, 12);
-			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(789, 606);
-			this->panel1->TabIndex = 0;
+			this->button1->Location = System::Drawing::Point(97, 216);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(174, 75);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Aide";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(353, 216);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(174, 75);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Connexion";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(97, 71);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(430, 22);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(97, 153);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(430, 22);
+			this->textBox2->TabIndex = 3;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(336, 353);
+			this->label1->Location = System::Drawing::Point(94, 116);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label1->Size = System::Drawing::Size(94, 17);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Mot de Passe";
+			this->label1->Click += gcnew System::EventHandler(this, &Connexion::label1_Click);
 			// 
-			// button1
+			// label2
 			// 
-			this->button1->Location = System::Drawing::Point(307, 464);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(146, 254);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(519, 22);
-			this->textBox1->TabIndex = 2;
-			// 
-			// comboBox1
-			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(285, 133);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(121, 24);
-			this->comboBox1->TabIndex = 3;
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(94, 20);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(69, 17);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Identifiant";
+			this->label2->Click += gcnew System::EventHandler(this, &Connexion::label2_Click);
 			// 
 			// Connexion
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(813, 630);
-			this->Controls->Add(this->panel1);
+			this->ClientSize = System::Drawing::Size(626, 303);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Connexion";
 			this->Text = L"Connexion";
-			this->panel1->ResumeLayout(false);
-			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
--- Form1.h
+++ Form1.h
@@ -64,6 +64,7 @@ namespace CppCLRWinformsProjekt {
 	private: System::Windows::Forms::Button^ button6;
 	private: System::Windows::Forms::Button^ button5;
 	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button7;
 
 
 
@@ -104,6 +105,7 @@ namespace CppCLRWinformsProjekt {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->Test = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
@@ -131,9 +133,11 @@ namespace CppCLRWinformsProjekt {
 			this->splitContainer1->Panel1->Controls->Add(this->button3);
 			this->splitContainer1->Panel1->Controls->Add(this->button2);
 			this->splitContainer1->Panel1->Controls->Add(this->button1);
+			this->splitContainer1->Panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::splitContainer1_Panel1_Paint);
 			// 
 			// splitContainer1.Panel2
 			// 
+			this->splitContainer1->Panel2->Controls->Add(this->button7);
 			this->splitContainer1->Panel2->Controls->Add(this->dataGridView1);
 			this->splitContainer1->Size = System::Drawing::Size(1359, 609);
 			this->splitContainer1->SplitterDistance = 240;
@@ -141,6 +145,8 @@ namespace CppCLRWinformsProjekt {
 			// 
 			// button6
 			// 
+			this->button6->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->button6->Location = System::Drawing::Point(3, 335);
 			this->button6->Name = L"button6";
 			this->button6->Size = System::Drawing::Size(235, 70);
@@ -150,29 +156,35 @@ namespace CppCLRWinformsProjekt {
 			// 
 			// button5
 			// 
+			this->button5->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->button5->Location = System::Drawing::Point(3, 259);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(235, 70);
 			this->button5->TabIndex = 4;
-			this->button5->Text = L"button5";
+			this->button5->Text = L"Magasin";
 			this->button5->UseVisualStyleBackColor = true;
 			// 
 			// button4
 			// 
+			this->button4->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->button4->Location = System::Drawing::Point(3, 183);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(235, 70);
 			this->button4->TabIndex = 3;
-			this->button4->Text = L"button4";
+			this->button4->Text = L"Gestionaire";
 			this->button4->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
+			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->button3->Location = System::Drawing::Point(3, 107);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(235, 70);
 			this->button3->TabIndex = 2;
-			this->button3->Text = L"button3";
+			this->button3->Text = L"Technicien";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// button2
@@ -181,7 +193,7 @@ namespace CppCLRWinformsProjekt {
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(115, 70);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"button2";
+			this->button2->Text = L"Logout";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button1
@@ -190,9 +202,19 @@ namespace CppCLRWinformsProjekt {
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(114, 70);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"User";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(966, 12);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(137, 56);
+			this->button7->TabIndex = 1;
+			this->button7->Text = L"Exit";
+			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &Form1::button7_Click);
 			// 
 			// dataGridView1
 			// 
@@ -205,11 +227,11 @@ namespace CppCLRWinformsProjekt {
 				this->Test, this->Column1,
 					this->Column2, this->Column3, this->Column4
 			});
-			this->dataGridView1->Location = System::Drawing::Point(3, 3);
+			this->dataGridView1->Location = System::Drawing::Point(3, 74);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
 			this->dataGridView1->RowTemplate->Height = 24;
-			this->dataGridView1->Size = System::Drawing::Size(1109, 603);
+			this->dataGridView1->Size = System::Drawing::Size(1109, 532);
 			this->dataGridView1->TabIndex = 0;
 			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Form1::dataGridView1_CellContentClick);
 			// 
@@ -254,6 +276,7 @@ namespace CppCLRWinformsProjekt {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1359, 609);
 			this->Controls->Add(this->splitContainer1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Form1";
 			this->Text = L"Commandes Composants";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
@@ -284,6 +307,12 @@ private: System::Void button4_Click_1(System::Object^ sender, System::EventArgs^
 private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void splitContainer1_Panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	Application::Exit();
 }
 };
 }
